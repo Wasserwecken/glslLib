48726fd789ac9463e88211582be64ab1938faa6e
Wasserwecken <eric.dolch@gmail.com>
2021-09-28 10:47:11 +0200

    finish fbm tests

--- tests/fbm.glsl
+++ tests/fbm.glsl
@@ -1,17 +1,23 @@
 #include "../lib/uv.glsl"
 #include "../lib/noise.fbm.glsl"
 
-float test_fbm_value(vec2 uv, float time)
+
+void main()
 {
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
+    vec2 uv, uvRatio;
+    uv_provide(gl_FragCoord.xy, iResolution.xy, uv, uvRatio);
 
-    vec3 point, seed;
+    vec2 tileUV, tileId;
+    uv_tilling(uv, vec2(3.0, 4.0), tileUV, tileId);
+    uv_fill(tileUV, uvRatio, tileUV);
+
+    vec3 point, seed, id, center, strength;
     int depth;
-    float smoothness, gain, scale;
+    float noise, smoothness, gain, scale, dist;
 
-    point = vec3(tile_uv, time * 0.02);
+    point = vec3(tileUV, iTime * 0.02);
     seed = vec3(floor(1.0 + iTime * 0.));
+    strength = vec3(sin(iTime * 0.5) * .5 + .5);
     smoothness = sin(iTime * 0.5) + 1.0;
 
     depth = 3;
@@ -19,115 +25,60 @@ float test_fbm_value(vec2 uv, float time)
     scale = 2.0;
 
 
-    switch(int(tile_id.x))
+    if (tileId.x < 1.0 && tileId.y < 1.0)
     {
-        case 0:
-            return noise_value(point.x, seed.x, smoothness, depth, gain, scale);
-        case 1:
-            return noise_value(point.xy, seed.xy, smoothness, depth, gain, scale);
-        case 2:
-            return noise_value(point.xyz, seed.xyz, smoothness, depth, gain, scale);
-    }
-}
-
-float test_fbm_perlin(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
-
-    vec3 point, seed;
-    int depth;
-    float gain, scale;
-
-    point = vec3(tile_uv, time * 0.02);
-    seed = vec3(floor(1.0 + iTime * 0.));
-
-    depth = 3;
-    gain = 0.5;
-    scale = 2.0;
-
-
-    switch(int(tile_id.x))
+        noise = noise_value(point.x, seed.x, smoothness, depth, gain, scale);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 1.0)
     {
-        case 0:
-            return noise_perlin(point.x, seed.x, depth, gain, scale);
-        case 1:
-            return noise_perlin(point.xy, seed.xy, depth, gain, scale);
-        case 2:
-            return noise_perlin(point.xyz, seed.xyz, depth, gain, scale);
-    }
-}
-
-float test_fbm_voronoi(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
-
-    vec3 noise, point, seed, id, center, strength;
-    int depth;
-    float gain, scale;
-
-    point = vec3(tile_uv, time * 0.02);
-    seed = vec3(floor(1.0 + iTime * 0.));
-    strength = vec3(sin(iTime * 0.5) * .5 + .5);
-
-    depth = 3;
-    gain = 0.5;
-    scale = 2.0;
-
-
-    switch(int(tile_id.x))
+        noise = noise_value(point.xy, seed.xy, smoothness, depth, gain, scale);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 1.0)
     {
-        case 0:
-            return noise_voronoi(point.x, seed.x, id.x, center.x, strength.x, depth, gain, scale);
-        case 1:
-            return noise_voronoi(point.xy, seed.xy, id.xy, center.xy, strength.xy, depth, gain, scale);
-        case 2:
-            return noise_voronoi(point.xyz, seed.xyz, id.xyz, center.xyz, strength.xyz, depth, gain, scale);
+        noise = noise_value(point.xyz, seed.xyz, smoothness, depth, gain, scale);
     }
-}
-
-float test_fbm_voronoi_edge(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
-
-    vec3 point, seed, id, center, strength;
-    int depth;
-    float gain, scale, dist;
-
-    point = vec3(tile_uv, time * 0.02);
-    seed = vec3(floor(1.0 + iTime * 0.));
-    strength = vec3(sin(iTime * 0.5) * .5 + .5);
-
-    depth = 3;
-    gain = 0.5;
-    scale = 2.0;
 
 
-    switch(int(tile_id.x))
+    else if (tileId.x < 1.0 && tileId.y < 2.0)
     {
-        case 1:
-            return noise_voronoi_edge(point.xy, seed.xy, id.xy, center.xy, dist, strength.xy, depth, gain, scale);
-        case 2:
-            return noise_voronoi_edge(point.xyz, seed.xyz, id.xyz, center.xyz, dist, strength.xyz, depth, gain, scale);
-    }
-}
-
-vec3 test_fbm(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(1.0, 4.0));
-
-    switch(int(tile_id.y))
+        noise = noise_perlin(point.x, seed.x, depth, gain, scale);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 2.0)
     {
-        case 0:
-            return vec3(test_fbm_value(tile_uv, time));
-        case 1:
-            return vec3(test_fbm_perlin(tile_uv, time));
-        case 2:
-            return vec3(test_fbm_voronoi(tile_uv, time));
-        case 3:
-            return vec3(test_fbm_voronoi_edge(tile_uv, time));
+        noise = noise_perlin(point.xy, seed.xy, depth, gain, scale);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 2.0)
+    {
+        noise = noise_perlin(point.xyz, seed.xyz, depth, gain, scale);
     }
+
+
+    else if (tileId.x < 1.0 && tileId.y < 3.0)
+    {
+        noise = noise_voronoi(point.x, seed.x, id.x, center.x, strength.x, depth, gain, scale);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 3.0)
+    {
+        noise = noise_voronoi(point.xy, seed.xy, id.xy, center.xy, strength.xy, depth, gain, scale);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 3.0)
+    {
+        noise = noise_voronoi(point.xyz, seed.xyz, id.xyz, center.xyz, strength.xyz, depth, gain, scale);
+    }
+
+
+    else if (tileId.x < 1.0 && tileId.y < 4.0)
+    {
+        noise = noise_voronoi(point.xy, seed.xy, id.xy, center.xy, strength.xy, depth, gain, scale);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 4.0)
+    {
+        noise = noise_voronoi_edge(point.xy, seed.xy, id.xy, center.xy, dist, strength.xy, depth, gain, scale);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 4.0)
+    {
+        noise = noise_voronoi_edge(point.xyz, seed.xyz, id.xyz, center.xyz, dist, strength.xyz, depth, gain, scale);
+    }
+
+	gl_FragColor = vec4(vec3(noise), 1.0);
 }
