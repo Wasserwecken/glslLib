03baba32b8261f2621c343cdd715f085c75df1e0
Wasserwecken <eric.dolch@gmail.com>
2021-09-28 10:53:25 +0200

    finish noise

--- tests/noise.glsl
+++ tests/noise.glsl
@@ -2,117 +2,79 @@
 #include "../lib/noise.glsl"
 
 
-#ifndef NOISE_TESTS
-#define NOISE_TESTS
-
-
-float test_noise_value(vec2 uv, float time)
+void main()
 {
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
+    vec2 uv, uvRatio;
+    uv_provide(gl_FragCoord.xy, iResolution.xy, uv, uvRatio);
 
-    vec3 point, seed;
-    float smoothness;
-
-    point = vec3(tile_uv, time * 0.02);
-    seed = vec3(floor(1.0 + iTime * 0.));
-    smoothness = sin(iTime * 0.5) + 1.0;
-
-    switch(int(tile_id.x))
-    {
-        case 0:
-            return noise_value(point.x, seed.x, smoothness);
-        case 1:
-            return noise_value(point.xy, seed.xy, smoothness);
-        case 2:
-            return noise_value(point.xyz, seed.xyz, smoothness);
-    }
-}
-
-float test_noise_perlin(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
-
-    vec3 point, seed;
-
-    point = vec3(tile_uv, time * 0.02);
-    seed = vec3(floor(1.0 + iTime * 0.));
-
-
-    switch(int(tile_id.x))
-    {
-        case 0:
-            return noise_perlin(point.x, seed.x);
-        case 1:
-            return noise_perlin(point.xy, seed.xy);
-        case 2:
-            return noise_perlin(point.xyz, seed.xyz);
-    }
-}
-
-float test_noise_voronoi(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
-
-    vec3 noise, point, seed, id, center, strength;
-
-    point = vec3(tile_uv, time * 0.02);
-    seed = vec3(floor(1.0 + iTime * 0.));
-    strength = vec3(sin(iTime * 0.5) * .5 + .5);
-
-
-    switch(int(tile_id.x))
-    {
-        case 0:
-            return noise_voronoi(point.x, seed.x, id.x, center.x, strength.x);
-        case 1:
-            return noise_voronoi(point.xy, seed.xy, id.xy, center.xy, strength.xy);
-        case 2:
-            return noise_voronoi(point.xyz, seed.xyz, id.xyz, center.xyz, strength.xyz);
-    }
-}
-
-float test_noise_voronoi_edge(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(4.0, 1.0));
+    vec2 tileUV, tileId;
+    uv_tilling(uv, vec2(3.0, 4.0), tileUV, tileId);
+    uv_fill(tileUV, uvRatio, tileUV);
 
     vec3 point, seed, id, center, strength;
-    float dist;
+    float smoothness, noise, dist;
 
-    point = vec3(tile_uv, time * 0.02);
+    point = vec3(tileUV, iTime * 0.02);
     seed = vec3(floor(1.0 + iTime * 0.));
+    smoothness = sin(iTime * 0.5) + 1.0;
     strength = vec3(sin(iTime * 0.5) * .5 + .5);
 
 
-    switch(int(tile_id.x))
+    if (tileId.x < 1.0 && tileId.y < 1.0)
     {
-        case 1:
-            return noise_voronoi_edge(point.xy, seed.xy, id.xy, center.xy, dist, strength.xy);
-        case 2:
-            return noise_voronoi_edge(point.xyz, seed.xyz, id.xyz, center.xyz, dist, strength.xyz);
-    }
-}
-
-
-vec3 test_noise(vec2 uv, float time)
-{
-    vec2 tile_id;
-    vec2 tile_uv = uv_tilling(uv, tile_id, vec2(1.0, 4.0));
-
-    switch(int(tile_id.y))
+        noise = noise_value(point.x, seed.x, smoothness);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 1.0)
     {
-        case 0:
-            return vec3(test_noise_value(tile_uv, time));
-        case 1:
-            return vec3(test_noise_perlin(tile_uv, time));
-        case 2:
-            return vec3(test_noise_voronoi(tile_uv, time));
-        case 3:
-            return vec3(test_noise_voronoi_edge(tile_uv, time));
+        noise = noise_value(point.xy, seed.xy, smoothness);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 1.0)
+    {
+        noise = noise_value(point.xyz, seed.xyz, smoothness);
     }
-}
 
-#endif
+
+    else if (tileId.x < 1.0 && tileId.y < 2.0)
+    {
+        noise = noise_perlin(point.x, seed.x);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 2.0)
+    {
+        noise = noise_perlin(point.xy, seed.xy);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 2.0)
+    {
+        noise = noise_perlin(point.xyz, seed.xyz);
+    }
+
+
+    else if (tileId.x < 1.0 && tileId.y < 3.0)
+    {
+        noise = noise_voronoi(point.x, seed.x, id.x, center.x, strength.x);
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 3.0)
+    {
+        noise = noise_voronoi(point.xy, seed.xy, id.xy, center.xy, strength.xy);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 3.0)
+    {
+        noise = noise_voronoi(point.xyz, seed.xyz, id.xyz, center.xyz, strength.xyz);
+    }
+
+
+    else if (tileId.x < 1.0 && tileId.y < 4.0)
+    {
+        noise = 0.0;
+    }    
+    else if (tileId.x < 2.0 && tileId.y < 4.0)
+    {
+        noise = noise_voronoi_edge(point.xy, seed.xy, id.xy, center.xy, dist, strength.xy);
+    }   
+    else if (tileId.x < 3.0 && tileId.y < 4.0)
+    {
+        noise = noise_voronoi_edge(point.xyz, seed.xyz, id.xyz, center.xyz, dist, strength.xyz);
+    }
+
+
+	gl_FragColor = vec4(vec3(noise), 1.0);
+}
